19f541b66b8ce85b1a496d001514b2db47f054f5
Ubuntu <835943102@qq.com>
2017-07-27 10:04:21 +0800

    count

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,2 +1,19 @@
 'use strict'
-
+let counted = (inputs)=> {
+  let allItems = loadAllItems();
+  let cartItems = [];
+  for (let input of inputs) {
+    let s = input.split('-');
+    let barcode = s[0];
+    let count = parseFloat(s[1] || 1);
+    let cartItem = cartItems.find((cartItem)=>cartItem.item.barcode == barcode);
+    if (cartItem) {
+      cartItem.count += count;
+    }
+    else {
+      let item=allItems.find((item)=>item.barcode==barcode);
+      cartItems.push({item:item,count:count});
+    }
+  }
+  return cartItems;
+}
--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -10,8 +10,44 @@ describe('pos_v1', () => {
       'ITEM000000',
       'ITEM000000',
       'ITEM000002-3',
-      'ITEM000005-4'
+      'ITEM000005-3'
     ];
   });
+
+
+  it('should return counted', ()=> {
+    const output = [
+      {
+        item: {
+          barcode: 'ITEM000000',
+          name: '可口可乐',
+          unit: '瓶',
+          price: 3.00
+        },
+        count: 4
+      },
+
+      {
+        item: {
+          barcode: 'ITEM000002',
+          name: '苹果',
+          unit: '斤',
+          price: 5.50
+        },
+        count: 3
+      },
+      {
+        item: {
+          barcode: 'ITEM000005',
+          name: '方便面',
+          unit: '袋',
+          price: 4.50
+        },
+        count: 3
+      }
+    ];
+    expect(counted(inputs)).toEqual(output);
+  });
+
 
 });
