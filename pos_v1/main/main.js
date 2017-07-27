9d68a0ab63e9d85719b4bb494366eba04e022a58
Ubuntu <835943102@qq.com>
2017-07-27 09:45:44 +0800

    test1

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,22 +1,2 @@
 'use strict'
-let printReceipt = (inputs)=> {
 
-}
-let count = (inputs)=> {
-  let allItems = loadAllItems();
-  let cartItems = [];
-  for (let input of inputs) {
-    let splited=input.split('-');
-    let barcode=splited[0];
-    let count=parseFloat(splited[1]||1);
-    let cartItem=cartItems.find((cartItem)=>cartItem.item.barcode==barcode);
-    if(cartItem){
-      cartItem.count+=count;
-    }
-    else{
-      let item=allItems.find((item)=>item.barcode==barcode);
-      cartItems.push({item:item,count:count});
-    }
-  }
-  return cartItems;
-}
